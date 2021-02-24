9f31508b4a45babf3c57993dc53b4f6c7b1c3209
BMoClay <benmorgancleveland@gmail.com>
2021-02-24 11:20:07 -0500

    app component running fetch to users

--- raregifs/src/App.js
+++ raregifs/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import React from 'react-router-dom';
+
 
 
 function App() {
+
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+
+    fetch('http://[::1]:3000/users')
+      .then(r => r.json())
+      .then(user => setUser(user))
+    }, [])  
+
+    console.log(user)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
