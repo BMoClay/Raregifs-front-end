4c5671b680be002c12b9afc11b93439bec9d3567
BMoClay <benmorgancleveland@gmail.com>
2021-03-07 17:04:37 -0500

    added conditionals to comments on collections

--- raregifs/src/Components/UserList.js
+++ raregifs/src/Components/UserList.js
@@ -13,21 +13,27 @@ function UserList({
             onDeleteComment, 
         }){
    
-    const collectionComments = comments.filter((comment) => comment.commentee_id === eachUser.id)
-    const thisCollection = acquisitions.filter((acquisition) => acquisition.user.id === eachUser.id)
+    const tCollection = 
+    eachUser.acquisitions ? (acquisitions.filter((acquisition) => acquisition.user.id === eachUser.id)
+    ) : (null)
     
-    const uAcquisitions =    
-        thisCollection.map((acquisition) => {
+    const uAcquisitions = 
+    eachUser.acquisitions ? (tCollection.map((acquisition) => {
             return <UserAcquisitionCard
                     key={acquisition.id}
                     aArtwork={acquisition.artwork}
                     cUId={cUId}
                     onAcquireArtwork={onAcquireArtwork}
                     />
-        })
-    
+            })
+    ) : (null)
+   
+    const tComments = 
+    eachUser.comments ? (comments.filter((comment) => comment.commentee_id === eachUser.id)
+    ) : (null)
+
     const uComments =
-        collectionComments.map((comment) => {
+    eachUser.comments ? (tComments.map((comment) => {
             return <CommentCard
                     key={comment.id}
                     cUId={cUId}
@@ -38,21 +44,71 @@ function UserList({
                     onDeleteComment={onDeleteComment}
                     />
         })
+    ) : (null)
 
     return(
         <div>
-            <h2>The {eachUser.name} Collection</h2>
-                {uAcquisitions}
-            <h3>Comments on {eachUser.name}'s collection</h3>    
-                {uComments}
-            <CommentForm 
-                eachUserID={eachUser.id} 
-                cUId={cUId} 
-                onAddComment={onAddComment}
-            />
-            <br></br>
+             {tCollection.length > 0 ? (
+                <>
+                    <h2>The {eachUser.name} Collection</h2>
+                        {uAcquisitions}
+                    <h3>Comments on {eachUser.name}'s collection</h3>    
+                        {uComments}
+                    <CommentForm 
+                        eachUserID={eachUser.id} 
+                        cUId={cUId} 
+                        onAddComment={onAddComment}
+                    />
+                    <br></br>
+                </>
+        ) : (
+            <>
+            </>
+        )}
         </div>
     );
 }
 
 export default UserList;
+
+// const collectionComments = comments.filter((comment) => comment.commentee_id === eachUser.id)
+//     const thisCollection = acquisitions.filter((acquisition) => acquisition.user.id === eachUser.id)
+    
+//     const uAcquisitions =    
+//         thisCollection.map((acquisition) => {
+//             return <UserAcquisitionCard
+//                     key={acquisition.id}
+//                     aArtwork={acquisition.artwork}
+//                     cUId={cUId}
+//                     onAcquireArtwork={onAcquireArtwork}
+//                     />
+//         })
+    
+//     const uComments =
+//         collectionComments.map((comment) => {
+//             return <CommentCard
+//                     key={comment.id}
+//                     cUId={cUId}
+//                     commentID={comment.id}
+//                     content={comment.content}
+//                     author={comment.author}
+//                     commenter_id={comment.commenter_id}
+//                     onDeleteComment={onDeleteComment}
+//                     />
+//         })
+
+//     return(
+//         <div>
+//             <h2>The {eachUser.name} Collection</h2>
+//                 {uAcquisitions}
+//             <h3>Comments on {eachUser.name}'s collection</h3>    
+//                 {uComments}
+//             <CommentForm 
+//                 eachUserID={eachUser.id} 
+//                 cUId={cUId} 
+//                 onAddComment={onAddComment}
+//             />
+//             <br></br>
+//         </div>
+//     );
+// }
