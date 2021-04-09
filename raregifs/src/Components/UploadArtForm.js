8a6f2a52497c8aa7a57bd0c1bef2e4f04f305854
BMoClay <benmorgancleveland@gmail.com>
2021-04-09 14:03:20 -0400

    function commented

--- raregifs/src/Components/UploadArtForm.js
+++ raregifs/src/Components/UploadArtForm.js
@@ -18,33 +18,33 @@ function UploadArtForm({ currentUser, onCreateArtwork }){
     const [title, setTitle] = useState("");
     const [image, setImage] = useState("");
 
-    const onDrop = useCallback((acceptedFiles) => {
-        const url = `https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUDINARY_CLOUD_NAME}/upload`;
+    // const onDrop = useCallback((acceptedFiles) => {
+    //     const url = `https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUDINARY_CLOUD_NAME}/upload`;
 
-        acceptedFiles.forEach(async (acceptedFile) => {
-            const { signature, timestamp } = await getSignature();
+    //     acceptedFiles.forEach(async (acceptedFile) => {
+    //         const { signature, timestamp } = await getSignature();
 
-            const formData = new FormData();
-            formData.append("file", acceptedFile);
-            // formData.append("upload_preset", process.env.REACT_APP_CLOUDINARY_UPLOAD_PRESET)
-            formData.append('signature', signature);
-            formData.append('timestamp', timestamp);
-            formData.append('api_key', process.env.REACT_APP_CLOUDINARY_KEY);
+    //         const formData = new FormData();
+    //         formData.append("file", acceptedFile);
+    //         // formData.append("upload_preset", process.env.REACT_APP_CLOUDINARY_UPLOAD_PRESET)
+    //         formData.append('signature', signature);
+    //         formData.append('timestamp', timestamp);
+    //         formData.append('api_key', process.env.REACT_APP_CLOUDINARY_KEY);
 
-            const response = await fetch(url, {
-                method: "post",
-                body: formData,
-            })
-            const data = await response.json();
-           setUploadedFiles(old => [...old, data]);
-        })
-    }, []);
+    //         const response = await fetch(url, {
+    //             method: "post",
+    //             body: formData,
+    //         })
+    //         const data = await response.json();
+    //        setUploadedFiles(old => [...old, data]);
+    //     })
+    // }, []);
 
-    const { getRootProps, getInputProps, isDragActive } = useDropzone({ 
-        onDrop, 
-        accepts: "image/*",
-        multiple: false,
-    })
+    // const { getRootProps, getInputProps, isDragActive } = useDropzone({ 
+    //     onDrop, 
+    //     accepts: "image/*",
+    //     multiple: false,
+    // })
 
     function handleSubmitNewArtwork(e) {
         e.preventDefault();
