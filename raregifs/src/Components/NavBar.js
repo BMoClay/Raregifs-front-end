bd40d370c27879f30cfe23bdceeb6f1aba7d6958
BMoClay <benmorgancleveland@gmail.com>
2021-07-02 10:29:10 -0400

    remove commented out code navbar

--- raregifs/src/Components/NavBar.js
+++ raregifs/src/Components/NavBar.js
@@ -51,36 +51,3 @@ function NavBar({ currentUser }) {
 
 export default NavBar
 
-// return( 
-//     <Dropdown
-//         icon='null'
-//         text="Raregifs" 
-//         // fluid 
-//         // floating 
-//         style={{
-//             width: '90px',
-//             fontSize: '1.2em',
-//             color: 'red',
-//             margin: '10px',
-//             //position: 'fixed',
-//         }}>
-//         <Dropdown.Menu text="Raregifs" style={{                 
-//             // position: 'fixed'
-//         }}>
-//             <Dropdown.Item text="Gallery" icon="trophy" as={Link} to='/'/>
-//             {currentUser ? (
-//                 <>
-//                     <Dropdown.Item text="Studio" icon="pencil" as={Link} to='/studio'/> 
-//                     <Dropdown.Item text="Storage" icon="shopping basket" as={Link} to='/my_storage'/>
-//                     <Dropdown.Item text="Collections" icon="university" as={Link} to='/users_collections'/>
-//                     <Dropdown.Item text="Account" icon="sign-out alternate" as={Link} to="/account"/>
-//                 </>
-//             ) : (
-//                 <>  
-//                     <Dropdown.Item text="Login" icon="sign-in" as={Link} to='/login'/>
-//                     <Dropdown.Item text="Signup" icon="signup" as={Link} to='/signup'/>
-//                 </>
-//             )}
-//         </Dropdown.Menu>
-//     </Dropdown>
-// )
