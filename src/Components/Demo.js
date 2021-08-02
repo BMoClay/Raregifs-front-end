babb761e7c89dce2d88526c5ac6df93c3a704adb
BMoClay <benmorgancleveland@gmail.com>
2021-08-02 13:24:50 -0400

    add text above demo vidoe

--- src/Components/Demo.js
+++ src/Components/Demo.js
@@ -3,24 +3,27 @@ import React from 'react';
 
 const Demo = () => {
     return (
-        <div 
-        className="video-responsive" 
-        // style={{
-        //         left: 0,
-        //         top: 0,
-        //         // height= 100%
-        //         // width= 100%
-        //         position: absolute,
-        //  }}
-         >
-            <iframe src='https://www.loom.com/embed/5a55e35216e54a198e4cf083a35c739c'
-            frameborder='0'
-            allow='autoplay; encrypted-media'
-            width="1000"
-            height="560"
-            allowfullscreen="true"
-            title='video'
-            />
+        <div className="demo">
+            <h4>quick 3 minute walk-through video</h4>
+            <div 
+            className="video-responsive" 
+            // style={{
+            //         left: 0,
+            //         top: 0,
+            //         // height= 100%
+            //         // width= 100%
+            //         position: absolute,
+            //  }}
+            >
+                <iframe src='https://www.loom.com/embed/5a55e35216e54a198e4cf083a35c739c'
+                frameborder='0'
+                allow='autoplay; encrypted-media'
+                width="1000"
+                height="560"
+                allowfullscreen="true"
+                title='video'
+                />
+            </div>
         </div>
     )
 }
