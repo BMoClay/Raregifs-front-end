52390899fe30758470920e0bee5ae8a63b29f48d
BMoClay <benmorgancleveland@gmail.com>
2021-08-02 10:12:42 -0400

    delete raregifs subdirectory

--- raregifs/src/client.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-axios.defaults.baseURL = process.env.REACT_APP_API_URL;
-// axios.defaults.baseURL = 'http://localhost:3000';
-const token = localStorage.getItem("token");
-if (token) {
-    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
-}
