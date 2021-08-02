0a555c16fe32b469eaaedfeaf9129d44962681f8
BMoClay <benmorgancleveland@gmail.com>
2021-08-02 08:28:39 -0400

    add _redirects file to public

--- src/client.js
+++ src/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 axios.defaults.baseURL = process.env.REACT_APP_API_URL;
-// axios.defaults.baseURL = 'http://localhost:3000';
+
 const token = localStorage.getItem("token");
 if (token) {
     axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
